089d6a65b9d205e69946dfc9a3da9fb9301d8d78
Ren <104393390+Renovsk@users.noreply.github.com>
2025-04-04 18:27:23 -0400

    added customizability to watermark as requested from mr.broshan

--- src/main/java/com/x310/clarity/hud/Watermark.java
+++ src/main/java/com/x310/clarity/hud/Watermark.java
@@ -1,16 +1,21 @@
 package com.x310.clarity.hud;
 
 import com.x310.clarity.Main;
+import meteordevelopment.meteorclient.settings.ColorSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.meteorclient.mixin.MinecraftClientAccessor;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.meteorclient.utils.world.TickRate;
 import net.minecraft.client.network.PlayerListEntry;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class Watermark extends HudElement {
+    private final SettingGroup sgGeneral = settings.createGroup("General");
     private static int ping() {
         if (mc.getNetworkHandler() == null || mc.player == null) return 0;
         PlayerListEntry playerListEntry = mc.getNetworkHandler().getPlayerListEntry(mc.player.getUuid());
@@ -21,6 +26,20 @@ public class Watermark extends HudElement {
     public Watermark() {
         super(INFO);
     }
+
+    private final Setting<SettingColor> lineColor = sgGeneral.add(((ColorSetting.Builder)((ColorSetting.Builder)(new ColorSetting.Builder())
+        .name("line color"))
+        .description("color wow"))
+        .defaultValue(new SettingColor())
+        .build()
+    );
+
+    private final Setting<SettingColor> textColor = sgGeneral.add(((ColorSetting.Builder)((ColorSetting.Builder)(new ColorSetting.Builder())
+        .name("text color"))
+        .description("color wow"))
+        .defaultValue(new SettingColor())
+        .build()
+    );
 
     @Override
     public void render(HudRenderer renderer) {
@@ -35,7 +54,8 @@ public class Watermark extends HudElement {
         int paddingX = 6;
         int paddingY = 3;
         int underlineHeight = 2;
-        int backgroundOffset = 3;
+        Color lcolor = lineColor.get();
+        Color tcolor = textColor.get();
 
         int outerWidth = textWidth + paddingX * 2;
         int outerHeight = textHeight + paddingY * 2;
@@ -44,9 +64,9 @@ public class Watermark extends HudElement {
 
 //        renderer.quad(x - backgroundOffset, y - backgroundOffset, outerWidth + backgroundOffset * 2, outerHeight + backgroundOffset * 2, new Color(50, 50, 50, 255));
         renderer.quad(x, y, outerWidth, outerHeight, new Color(0, 0, 0, 255));
-        renderer.quad(x, y - underlineHeight, outerWidth, underlineHeight, new Color(255, 255, 255, 255));
-        renderer.text(text, x + paddingX, y + paddingY, new Color(255, 255, 255, 255), true);
-        renderer.text("clarity.gay", x + paddingX, y + paddingY, new Color(255, 255, 255, 255), true);
+        renderer.quad(x, y - underlineHeight, outerWidth, underlineHeight, lcolor);
+        renderer.text(text, x + paddingX, y + paddingY, tcolor, true);
+        renderer.text("clarity.gay", x + paddingX, y + paddingY, tcolor, true);
     }
 
 }
