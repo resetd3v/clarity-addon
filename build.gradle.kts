d290ef077f7708bfeb74f58924e9c889533dbb90
0x310 <104393390+Renovsk@users.noreply.github.com>
2025-04-06 09:13:57 -0400

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     minecraft("com.mojang:minecraft:${properties["minecraft_version"] as String}")
     mappings("net.fabricmc:yarn:${properties["yarn_mappings"] as String}:v2")
     modImplementation("net.fabricmc:fabric-loader:${properties["loader_version"] as String}")
+	modImplementation("net.fabricmc.fabric-api:fabric-networking-api-v1:4.4.0+db5e668204")
 
     // Meteor
     modImplementation("meteordevelopment:meteor-client:${properties["minecraft_version"] as String}-SNAPSHOT")
